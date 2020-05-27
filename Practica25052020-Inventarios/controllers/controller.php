cd7490656d122f629b26f84b2bed13da911fee88
Ramiro Cedillo <1730091@upv.edu.mx>
2020-05-27 08:31:19 -0500

    Metodo insertar en el controller

--- Practica25052020-Inventarios/controllers/controller.php
+++ Practica25052020-Inventarios/controllers/controller.php
@@ -67,23 +67,23 @@
 						<form method="post" action="index.php?action=usuarios">
 							<div class="form-group">
 								<label for="nusuariotxt">Nombre:</label>
-								<input  class="form-control" type="text" name="nusuariotxt" id="nusuariotxt">
+								<input  class="form-control" type="text" name="nusuariotxt" id="nusuariotxt" required>
 							</div>
 							<div class="form-group">
 								<label for="ausuariotxt">Apellidos:</label>
-								<input  class="form-control" type="text" name="ausuariotxt" id="ausuariotxt">
+								<input  class="form-control" type="text" name="ausuariotxt" id="ausuariotxt" required>
 							</div>
 							<div class="form-group">
 								<label for="usuariotxt">Usuario:</label>
-								<input  class="form-control" type="text" name="usuariotxt" id="usuariotxt">
+								<input  class="form-control" type="text" name="usuariotxt" id="usuariotxt" required> 
 							</div>
 							<div class="form-group">
 								<label for="ucontratxt">Contraseña:</label>
-								<input  class="form-control" type="password" name="ucontratxt" id="ucontratxt">
+								<input  class="form-control" type="password" name="ucontratxt" id="ucontratxt" required>
 							</div>
 							<div class="form-group">
 								<label for="uemailtxt">Correo Electornico:</label>
-								<input  class="form-control" type="password" name="uemailtxt" id="uemailtxt">
+								<input  class="form-control" type="password" name="uemailtxt" id="uemailtxt" required>
 							</div>
 							<button class="btn btn-primary" type="submit">Agregar</button>
 						</form>
@@ -91,6 +91,47 @@
 				</div>
 			</div>
 			<?php
+		}
+
+		/*Este contorlador sirve para insertar el usuario que se acaba de ingresar y notifica si se rlaizo dicha actividad o si hubo algun error, en el caso de la contsaseña, primero que nada se tendra uqe encriptar mediante el algoritmo hash y la funcion password_hash y se guarda para posteriromente realizar la insercion*/
+		public function insertarUsuarioController(){
+			if(isset($_POST["nusuariotxt"])){
+				//encriptar la contraseña
+				$_POST["ucontratxt"]=password_hash($_POST["ucontratxt"], PASSWORD_DEFAULT);
+
+				//Almacenar en un array los valores de los text del metodo "registrarUserController"
+				$datosController=array("nusuario"=>$_POST["nusuariotxt"],"ausuario"=>$_POST["usuariotxt"],"usuario"=>$_POST["usuariotxt"],"ucontra"=>$_POST["ucontratxt"],"nemailtxt"=>$_POST["nemailtxt"]);
+
+				$respuesta=Datos::insertarUsuarioModel($datosController,"users");
+
+				if ($respuesta=="success") {
+					echo ' 
+						<div class="col-md-6 mt-3">
+							<div class="alert alert-success alert-dismissible">
+								<button class="close" type="button" data-miss="alert" aria-hidden="true">x</button>
+								<h5>
+									<i class="icon fas fa-check"></i>
+									Exito!
+								</h5>
+								Usuario agregado con éxito.
+							</div>
+						</div>
+					';
+				}else{
+					echo ' 
+						<div class="col-md-6 mt-3">
+							<div class="alert alert-danger alert-dismissible">
+								<button class="close" type="button" data-miss="alert" aria-hidden="true">x</button>
+								<h5>
+									<i class="icon fas fa-ban"></i>
+									Error!
+								</h5>
+								Error.
+							</div>
+						</div>
+					';
+				}
+			}
 		}	
 	}
 
