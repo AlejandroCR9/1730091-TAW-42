30d2f3ad6f2f15a03d6cd347ca5c37e4df351a9d
Ramiro Alejandro Cedillo Rubalcava <35510800+AlejandroCR9@users.noreply.github.com>
2020-06-12 08:56:22 -0500

    Update conexion.php

--- Practica25052020-Inventarios/models/conexion.php
+++ Practica25052020-Inventarios/models/conexion.php
@@ -1,7 +1,7 @@
 <?php
 	class Conexion{
 		public static function conectar(){
-			$link = new PDO("mysql:host=localhost;dbname=inventarios","root","");
+			$link = new PDO("mysql:host=localhost;dbname=inventarios","admin","770056391dda2ccecf16b57f70a278120f81e8f74c571e94");
 			return $link;
 		}
 	}
