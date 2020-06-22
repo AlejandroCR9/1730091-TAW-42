12bd6b0a493161d2610e9e1f98bef4104e1fc8bd
Ramiro Cedillo <1730091@upv.edu.mx>
2020-06-21 23:43:44 -0500

    Rutas en api.php

--- PracticaL18062020/routes/api.php
+++ PracticaL18062020/routes/api.php
@@ -2,18 +2,45 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Empleados;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Listar Empleados
+Route::get('empleados', function(){
+    $empleados = Empleados::get();
+    return $empleados;
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('empleados', function(Request $request){
+    //Verificamos que los datos enviados se reciban bien  para guardar en la bd, utilizando Request
+    //Retirnar un solo parametro
+    //return $requiset->input('estado_civil');
+    //retornar todos los valores del array del form elaborado en postam
+
+    //return $request->all();
+    //Validar data de empleados:
+    $request->validate([
+        'nombres' => 'required|max=50',
+        'apellidos' => 'required|max=50',
+        'cedula' => 'required|max=20',
+        'email' => 'required|max=50|email|unique:empleados',
+        'lugar_nacimiento' => 'required|max=50',
+        'estado_civil' => 'required|max=50',
+        'telefono' => 'required|numeric'
+        
+    ]);
+
+    //Llenar los parametros usando el Request
+    $empleado = new Empleado;
+    $empleado->nombres=$request->input('nombres');
+    $empleado->apellidos=$request->input('apellidos');
+    $empleado->cedula=$request->input('cedula');
+    $empleado->email=$request->input('email');
+    $empleado->lugar_nacimiento=$request->input('lugar_nacimiento');
+    $empleado->sexo=$request->input('sexo');
+    $empleado->estado_civil=$request->input('estado_civil');
+    $empleado->telefono=$request->input('telefono');
+    $empleado->save();
+
+    return 'Usuario creado';
+
 });
