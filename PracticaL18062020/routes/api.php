f82c4bb31aacbfe5c25fa5865be8ab7add1a8aef
Ramiro Cedillo <1730091@upv.edu.mx>
2020-06-22 08:49:18 -0500

    Metedos actualizar, crear y eliminar empleado

--- PracticaL18062020/routes/api.php
+++ PracticaL18062020/routes/api.php
@@ -10,6 +10,7 @@ Route::get('empleados', function(){
     return $empleados;
 });
 
+//Crear empleados
 Route::post('empleados', function(Request $request){
     //Verificamos que los datos enviados se reciban bien  para guardar en la bd, utilizando Request
     //Retirnar un solo parametro
@@ -43,4 +44,41 @@ Route::post('empleados', function(Request $request){
 
     return 'Usuario creado';
 
+});
+
+//Actualizar
+//Ruta para actualizar empleados
+Route::put('empleados/{id}', function(Request $request, $id){
+
+    //Validación de los campos
+    $request->validate([
+        'nombres' => 'required|max=50',
+        'apellidos' => 'required|max=50',
+        'cedula' => 'required|max=20',
+        'email' => 'required|max=50|email|unique:empleados,email,'.$id,
+        'lugar_nacimiento' => 'required|max=50',
+        'estado_civil' => 'required|max=50',
+        'telefono' => 'required|numeric'
+        
+    ]);
+
+    //Se busca el empleado en la bd, sino genera un error
+    $empleado = Empleado::findOrFail($id);
+
+    $empleado->nombres=$request->input('nombres');
+    $empleado->apellidos=$request->input('apellidos');
+    $empleado->cedula=$request->input('cedula');
+    $empleado->email=$request->input('email');
+    $empleado->lugar_nacimiento=$request->input('lugar_nacimiento');
+    $empleado->sexo=$request->input('sexo');
+    $empleado->estado_civil=$request->input('estado_civil');
+    $empleado->telefono=$request->input('telefono');
+    $empleado->save();
+    return 'Empleado actualizado';
+});
+
+Route::delete('empleados/{id}', function($id){
+    $empleado=Empleado::findOrFail($id);
+    $empleado->delete();
+    return 'Empleado eliminado';
 });
